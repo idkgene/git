64f0b65ebb2d016f823aa0ed1f52f9f9a276f7e3
Eugene <webeugene@proton.me>
2024-11-09 10:51:25 +0300

    feat: rm() function which removes files that match path from the index.

--- script.js
+++ script.js
@@ -44,4 +44,44 @@ var gitty = (module.exports = {
       })
     }
   },
+
+  rm: function (path, opts) {
+    files.assertInRepo()
+    config.assertNotBare()
+    opts = opts || {}
+
+    var filesToRm = index.matchingFiles(path)
+
+    if (opts.f) {
+      throw new Error('rm -f not supported yet')
+    } else if (filesToRm.length === 0) {
+      throw new Error(
+        filesToRm.pathFromRepoRoot(path) + 'did not match any files',
+      )
+    } else if (
+      fs.existsSync(path) &&
+      fs.statSync(path).isDirectory() &&
+      !opts.r
+    ) {
+      throw new Error('not removing ' + path + ' recursively without -r')
+    } else {
+      var changesToRm = util.intersection(
+        diff.addedOrModifiedFiles(),
+        filesToRm,
+      )
+      if (changesToRm.length > 0) {
+        throw new Error(
+          'these files have changes:\n' + changesToRm.join('\n') + '\n',
+        )
+      } else {
+        filesToRm
+          .map(files.workingCopyPath)
+          .filter(fs.existsSync)
+          .forEach(fs.unlinkSync)
+        filesToRm.forEach(function (p) {
+          gitty.update_index(p, { remove: true })
+        })
+      }
+    }
+  },
 })
