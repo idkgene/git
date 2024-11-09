b0f1ff691dfc85a087ceabb092d0654f9c3f8fe4
Eugene <webeugene@proton.me>
2024-11-09 10:45:15 +0300

    feat: init() function
    init() initializes the current directory as a new repository.
    Abort if alread a repository.
    Create a JS object that mirrors the Git basic dir structure.
    `--bare` option
    gittyStructure object

--- script.js
+++ script.js
@@ -1,4 +1,30 @@
 #!/usr/bin/env node
 
-var fs = require('fs');
-var nodePath = require('path');
+var fs = require('fs')
+var nodePath = require('path')
+
+var gitty = (module.exports = {
+  init: function (opts) {
+    if (files.inRepo()) {
+      return
+    }
+
+    opts = opts || {}
+
+    var gittyStructure = {
+      HEAD: 'ref: refs/heads/master\n',
+
+      config: config.objToStr({ core: { '': { bare: opts.bare === true } } }),
+
+      objects: {},
+      refs: {
+        heads: {},
+      },
+    }
+
+    files.writeFilesFromTree(
+      opts.bare ? gittyStructure : { '.gitty': gittyStructure },
+      process.cwd(),
+    )
+  },
+})
